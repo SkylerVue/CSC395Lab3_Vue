8f3659f486b27ffff1f0eba40dc12d8d7960ee51
Godwill Exercices Solutions <afolabigodwill24@gmail.com>
2023-01-31 11:49:29 -0600

    changes

---
+++ lab03_observer/NewsFeed.java
@@ -0,0 +1,16 @@
+public class NewsFeed implements WeatherObserver, StockObserver{
+
+    @Override
+    public void update(Integer temp, Integer precip, Integer air) {
+        // TODO Auto-generated method stub
+        
+    }
+    @Override
+    public void update(Double dow, Double snp, Double mmm, Double msft){
+
+    }
+
+    public void display(){
+    }
+
+} 
